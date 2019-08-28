3ec879ea6b62e6b74ca14f94db83fa2bfd25cae0
spring <qianchunhua@ly-sky.com>
2019-08-28 11:10:38 +0800

    SpringBoot 2.x学习项目初始化

---
+++ src/main/java/cn/itspring/cloud/SpringBoot2xApplication.java
@@ -0,0 +1,13 @@
+package cn.itspring.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot2xApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot2xApplication.class, args);
+    }
+
+}
